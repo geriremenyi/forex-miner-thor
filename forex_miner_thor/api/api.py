6ac1a4889da953de3e2e1b54fc9c5cd9ac4cb0d8
Gergely Reményi <geri@geriremenyi.com>
2020-10-17 21:54:05 +0200

    Adding backtesting library and engine interface

--- forex_miner_thor/api/api.py
+++ forex_miner_thor/api/api.py
@@ -4,6 +4,7 @@ from marshmallow import ValidationError
 
 from forex_miner_thor.api.error import ProblemDetailsSchema, ProblemDetails
 from forex_miner_thor.api.tick import TickInstrumentSchema
+from forex_miner_thor.api.trade import TradeSignalSchema, TradeSignal, TradeDirection
 
 # Setup flask
 api = Flask(__name__)
@@ -19,7 +20,7 @@ def tick():
 
     try:
         instruments = TickInstrumentSchema(many=True).load(request.get_json())
-        return jsonify(TickInstrumentSchema(many=True).dump(instruments))
+        return jsonify(TradeSignalSchema().dump(TradeSignal(instruments[0].instrument, TradeDirection.LONG, 1.25, 1.30)))
     except ValidationError as ex:
         return ProblemDetailsSchema().dump(ProblemDetails(
             422, ex.messages
---
+++ forex_miner_thor/api/trade/__init__.py
@@ -0,0 +1,4 @@
+from forex_miner_thor.api.trade.trade_direction import TradeDirection
+from forex_miner_thor.api.trade.trade_signal import TradeSignal
+from forex_miner_thor.api.trade.trade_signal_schema import TradeSignalSchema
+
