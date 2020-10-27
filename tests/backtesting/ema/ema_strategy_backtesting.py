4ddefb30697aa90015d917175ecc22ea5a81d473
Gergely Reményi <geri@geriremenyi.com>
2020-10-27 22:01:00 +0200

    Optimizing trading via backtesting

--- tests/backtesting/ema/ema_strategy_backtesting.py
+++ tests/backtesting/ema/ema_strategy_backtesting.py
@@ -1,28 +1,37 @@
-from backtesting import Strategy
-from backtesting.lib import crossover
+import pandas as pd
+from backtesting.lib import SignalStrategy, TrailingStrategy
 
 from forex_miner_thor.strategies.ema.ema import ema
 
 
-class EmaStrategyBackTesting(Strategy):
+class EmaStrategyBackTesting(SignalStrategy, TrailingStrategy):
     n1 = 10
     n2 = 20
+    percentile = 75
+    position_size_percentage = 10
+    stop_loss = 2
 
     def init(self):
+        # Init composed strategies
+        super().init()
+
         # Precompute the two moving averages
         self.ema1 = self.I(ema, self.data.Close, self.n1)
         self.ema2 = self.I(ema, self.data.Close, self.n2)
 
-    def next(self):
-        # If sma1 crosses above sma2, close any existing
-        # short trades, and buy the asset
-        if crossover(self.ema1, self.ema2):
-            self.position.close()
-            self.buy()
+        # Precompute volume percentile
+        volume_percentile = pd.Series(self.data.Volume).quantile((self.percentile * 0.01))
 
-        # Else, if sma1 crosses below sma2, close any existing
-        # long trades, and sell the asset
-        elif crossover(self.ema1, self.ema2):
-            self.position.close()
-            self.sell()
+        # Cross signal
+        cross = (pd.Series(self.ema1) > self.ema2).astype(int).diff().fillna(0)
+
+        # Volume signal
+        volume = pd.Series((pd.Series(self.data.Volume) > volume_percentile)).astype(int).fillna(0)
+
+        # Use 10% of the liquid balance for each trade
+        entry_size = cross * volume * (self.position_size_percentage * 0.01)
+
+        # Use the strategies implemented
+        self.set_signal(entry_size)
+        self.set_trailing_sl(self.stop_loss)
 
