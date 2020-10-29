b4c094ac13bdb5f4f43c14b9219a567a7b331978
Gergely Reményi <geri@geriremenyi.com>
2020-10-29 18:55:00 +0200

    Adding API tests

--- forex_miner_thor/api/api.py
+++ forex_miner_thor/api/api.py
@@ -11,70 +11,71 @@ from forex_miner_thor.utilities.add_instrument_data import add_instrument_data
 from forex_miner_thor.strategies import apply_strategies
 
 
-# Setup flask
-api = Flask(__name__)
+def create_api():
+    # Setup flask
+    api = Flask(__name__)
+
+    # Default endpoint just to report back 200
+    @api.route('/api/v1/engine', methods=['GET'])
+    def report_status():
+        return '', 200
+
+    # Tick route
+    @api.route('/api/v1/engine/tick', methods=['POST'])
+    def tick():
+        if not request.is_json:
+            return (
+                ProblemDetailsSchema().dump(ProblemDetails(415, "Only 'application/json' is a supported 'Content-Type'.")),
+                415
+            )
+
+        try:
+            # Empty trade signals
+            trade_signals: List[TradeSignal] = []
+
+            # Process instruments and apply strategy on them
+            instruments: List[Instrument] = InstrumentSchema(many=True).load(request.get_json())
+            for inst in instruments:
+                candles = add_instrument_data(inst.instrument, inst.granularity, inst.candles)
+                trade_signal = apply_strategies(inst.instrument, candles)
+                if trade_signal is not None:
+                    trade_signals.append(trade_signal)
+
+            # Return trade signals
+            return jsonify(TradeSignalSchema(many=True).dump(trade_signals)), 200
+        except ValidationError as vex:
+            return ProblemDetailsSchema().dump(ProblemDetails(422, vex.messages)), 422
+        except Exception as ex:
+            return ProblemDetailsSchema().dump(ProblemDetails(500, str(ex))), 500
+
+    # Add instrument data route
+    @api.route('/api/v1/engine/instruments/<instrument>/granularities/<granularity>', methods=['POST'])
+    def post_instrument_granularity_candles(instrument: str, granularity: str):
+        # Error handling on non json data
+        if not request.is_json:
+            return (
+                ProblemDetailsSchema().dump(ProblemDetails(415, "Only 'application/json' is a supported 'Content-Type'.")),
+                415
+            )
+
+        try:
+            # Parse incoming candles
+            candles = CandleSchema(many=True).load(request.get_json())
+
+            # Add data to csv files
+            add_instrument_data(instrument, granularity, candles)
+
+            # Return 201
+            return '', 201
+        except ValidationError as vex:
+            return ProblemDetailsSchema().dump(ProblemDetails(422, vex.messages)), 422
+        except Exception as ex:
+            return ProblemDetailsSchema().dump(ProblemDetails(500, str(ex))), 500
+
+    return api
 
 
-# Default endpoint just to report back 200
-@api.route('/api/v1/engine', methods=['GET'])
-def report_status():
-    return '', 200
-
-
-# Tick route
-@api.route('/api/v1/engine/tick', methods=['POST'])
-def tick():
-    if not request.is_json:
-        return (
-            ProblemDetailsSchema().dump(ProblemDetails(415, "Only 'application/json' is a supported 'Content-Type'.")),
-            415
-        )
-
-    try:
-        # Empty trade signals
-        trade_signals: List[TradeSignal] = []
-
-        # Process instruments and apply strategy on them
-        instruments: List[Instrument] = InstrumentSchema(many=True).load(request.get_json())
-        for inst in instruments:
-            candles = add_instrument_data(inst.instrument, inst.granularity, inst.candles)
-            trade_signal = apply_strategies(inst.instrument, candles)
-            if trade_signal is not None:
-                trade_signals.append(trade_signal)
-
-        # Return trade signals
-        return jsonify(TradeSignalSchema(many=True).dump(trade_signals)), 200
-    except ValidationError as vex:
-        return ProblemDetailsSchema().dump(ProblemDetails(422, vex.messages)), 422
-    except Exception as ex:
-        return ProblemDetailsSchema().dump(ProblemDetails(500, str(ex))), 500
-
-
-# Add instrument data route
-@api.route('/api/v1/engine/instruments/<instrument>/granularities/<granularity>', methods=['POST'])
-def post_instrument_granularity_candles(instrument: str, granularity: str):
-    # Error handling on non json data
-    if not request.is_json:
-        return (
-            ProblemDetailsSchema().dump(ProblemDetails(415, "Only 'application/json' is a supported 'Content-Type'.")),
-            415
-        )
-
-    try:
-        # Parse incoming candles
-        candles = CandleSchema(many=True).load(request.get_json())
-
-        # Add data to csv files
-        add_instrument_data(instrument, granularity, candles)
-
-        # Return 201
-        return '', 201
-    except ValidationError as vex:
-        return ProblemDetailsSchema().dump(ProblemDetails(422, vex.messages)), 422
-    except Exception as ex:
-        return ProblemDetailsSchema().dump(ProblemDetails(500, str(ex))), 500
-
-
-# Run API
-api.run(host='0.0.0.0', port=31001)
+if __name__ == "__main__":
+    api = create_api()
+    api.run(host='0.0.0.0', port=31001)
 
