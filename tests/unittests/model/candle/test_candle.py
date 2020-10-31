a62ab2096e7f73281a71945627177a42e6c0df0c
Gergely Reményi <geri@geriremenyi.com>
2020-10-31 09:27:00 +0200

    Adding candlestick tests

--- tests/unittests/model/candle/test_candle.py
+++ tests/unittests/model/candle/test_candle.py
@@ -23,6 +23,24 @@ class TestCandle(unittest.TestCase):
         self.assertEqual(candle.mid, mid)
         self.assertEqual(candle.ask, ask)
 
+    def test_to_dict(self):
+        # Arrange
+        time = datetime.datetime.now()
+        volume = 12345
+        bid = Candlestick(1.2, 1.3, 1.0, 1.1)
+        mid = Candlestick(1.3, 1.4, 1.1, 1.2)
+        ask = Candlestick(1.4, 1.5, 1.2, 1.3)
+
+        # Act
+        candle_dict = Candle(time, volume, bid, mid, ask).to_dict()
+
+        self.assertEqual(candle_dict['Time'], time)
+        self.assertEqual(candle_dict['Volume'], volume)
+        self.assertEqual(candle_dict['Open'], mid.open)
+        self.assertEqual(candle_dict['High'], mid.high)
+        self.assertEqual(candle_dict['Low'], mid.low)
+        self.assertEqual(candle_dict['Close'], mid.close)
+
 
 if __name__ == '__main__':
     unittest.main()
