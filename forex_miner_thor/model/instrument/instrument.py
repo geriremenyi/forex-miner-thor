34adc6e57d8152bdf87f9b7901e8db641c69c04f
Gergely Reményi <geri@geriremenyi.com>
2020-11-01 12:11:00 +0200

    Adding broken instrument fixes

--- forex_miner_thor/model/instrument/instrument.py
+++ forex_miner_thor/model/instrument/instrument.py
@@ -4,12 +4,7 @@ from forex_miner_thor.model.candle import Candle
 
 
 class Instrument(object):
-    def __init__(
-            self,
-            instrument: str,
-            granularity: str,
-            candles: List[Candle]
-    ):
+    def __init__(self, instrument: str, granularity: str, candles: List[Candle]):
         self.instrument = instrument
         self.granularity = granularity
         self.candles = candles
